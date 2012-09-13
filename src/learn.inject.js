7a54052053ab24e010eef5bed93a876d47a90aaa
UGeeker <xxr3376@gmail.com>
2012-09-13 23:23:31 +0800

    fix Background of clicked tab

---
+++ src/learn.inject.js
@@ -0,0 +1,12 @@
+
+        window.NN_showImage = function(num, k) {
+            for (var i = 1; i <= num; ++i) {
+                var obj = document.getElementById("ImageTab"+i);
+                if (i != k) {
+                    obj.style.background = 'url("/images/new/Y_bg_no.gif")';
+                } else {
+                    obj.style.background = 'url("/images/new/Y_bg_on.gif")';
+                }
+
+            }
+        };
--- src/learn.js
+++ src/learn.js
@@ -1,15 +1,23 @@
 (function() {
-	 "use strict";
+    "use strict";
 
-	if (window.location.href.match(/download\.jsp/)) {
-		var e = document.getElementById('table_box');
-		e.style.position = "absolute";
-		e.style.top = "-30px";
-	}
-	else if (window.location.href.match(/talk_list_student\.jsp/)){
-		var e = document.querySelector('#info_1 tr:nth-child(4) table:nth-child(2)');
-		e.style.position = "relative";
-		e.style.top = "-26px";
-	}
- })();
+    var injectScript = function() {
+        var s = document.createElement('script');
+        s.src = chrome.extension.getURL("learn.inject.js");
+        document.head.appendChild(s);
+    };
 
+    var href= window.location.href;
+    if (href.match(/student\/download\.jsp/)) {
+        var e = document.getElementById('table_box');
+        e.style.position = "absolute";
+        e.style.top = "-30px";
+        injectScript();
+    } else if (href.match(/bbs\/talk_list_student\.jsp/)) {
+        var e = document.querySelector('#info_1 tr:nth-child(4) table:nth-child(2)');
+        e.style.position = "relative";
+        e.style.top = "-26px";
+        injectScript();
+    }
+
+})();
