c8efd1960bfebc829fdb416c0843032f5f406f37
UGeeker <xxr3376@gmail.com>
2012-09-14 09:04:25 +0800

    add deadline hyperlink

---
+++ src/main.js
@@ -0,0 +1,4 @@
+var tr = document.createElement("tr")
+document.querySelector("#left_menu > tbody").appendChild(tr)
+tr.innerHTML = '<td class="menu_common"><a target="_blank" href="/defSet.jsp">Deadline！！！！</a></td>'
+
