8021d8dfa073b60fe1bacb08ea8c546431a16026
brian14708 <brian14708@gmail.com>
2012-09-25 09:59:26 +0800

    !

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-var tr_deadline = document.createElement("tr")
-document.querySelector("#left_menu > tbody").appendChild(tr_deadline)
-var ref = chrome.extension.getURL("deadline.html")
-tr_deadline.innerHTML = '<td class="menu_common"><a target="_blank" href="' + ref + '">Deadline！！！！</a></td>'
+var tr_deadline = document.createElement("tr");
+var ref = chrome.extension.getURL("deadline.html");
+tr_deadline.innerHTML = '<td class="menu_common"><a target="_blank" href="' + ref + '">Deadline</a></td>';
 
-var tr_notification = document.createElement("tr")
-document.querySelector("#left_menu > tbody").appendChild(tr_notification)
-ref = chrome.extension.getURL("notification.html")
-tr_notification.innerHTML = '<td class="menu_common"><a target="_blank" href="' + ref + '">Notification！！！！</a></td>'
+var tr_notification = document.createElement("tr");
+ref = chrome.extension.getURL("notification.html");
+tr_notification.innerHTML = '<td class="menu_common"><a target="_blank" href="' + ref + '">Notification</a></td>';
+
+var menu = document.querySelector("#left_menu > tbody");
+menu.appendChild(tr_deadline);
+menu.appendChild(tr_notification);
 
