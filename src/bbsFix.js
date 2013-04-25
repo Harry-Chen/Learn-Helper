1c152c759d8fc10fdc41360ed05a1aee34106faf
brian14708 <brian14708@gmail.com>
2013-04-25 12:58:20 +0800

    bbs

--- src/bbsFix.js
+++ src/bbsFix.js
@@ -1,6 +1,8 @@
 $(function(){
 	$('.tr_l2').each(function() {
-		if (!this.innerHTML.match(/<[a-zA-Z]+[^>]*>/))
-			$(this).wrapInner('<pre style="width:700px"/>');
+		if (!this.innerHTML.match(/<[a-zA-Z]+[^>]*>/)) {
+      var $this = $(this);
+			$this.wrapInner('<pre style="width:700px;max-height:' + $this.height() + 'px;overflow:auto"/>');
+    }
 	});
 });
