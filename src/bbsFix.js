d3f43e76ec5cd3a200a5d5be9b0a4066ab62c98c
brian14708 <brian14708@gmail.com>
2013-04-25 12:59:42 +0800

    tabs...

--- src/bbsFix.js
+++ src/bbsFix.js
@@ -1,8 +1,8 @@
 $(function(){
 	$('.tr_l2').each(function() {
 		if (!this.innerHTML.match(/<[a-zA-Z]+[^>]*>/)) {
-      var $this = $(this);
+			var $this = $(this);
 			$this.wrapInner('<pre style="width:700px;max-height:' + $this.height() + 'px;overflow:auto"/>');
-    }
+		}
 	});
 });
