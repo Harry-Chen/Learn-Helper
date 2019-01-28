1b35f114cb87d6d83ed7132a652df4c659d78e07
Harry Chen <i@harrychen.xyz>
2019-01-28 18:11:46 +0800

    New project for learn 2018
    Signed-off-by: Harry Chen <i@harrychen.xyz>

---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    background: path.join(__dirname, "src/background.ts")
+  },
+  output: {
+    path: path.join(__dirname, "dist/js"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // Creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // Translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // Compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  }
+};
