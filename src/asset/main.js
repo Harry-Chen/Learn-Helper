d84b13444937348e5d9c6a41650b1e9a5a7a735b
Harry Chen <i@harrychen.xyz>
2019-01-28 18:11:46 +0800

    New project for learn 2018
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/asset/main.js
+++
@@ -1,7 +0,0 @@
-var tr_options = document.createElement("tr");
-ref = chrome.extension.getURL("index.html");
-tr_options.innerHTML = '<td class="menu_common"><a target="_blank" href="' + ref + '">Learn Helper</a></td>';
-
-var menu = document.querySelector("#left_menu > tbody");
-
-menu.appendChild(tr_options);
