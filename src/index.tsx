312b4a9a8476e14cb925d6531e4da0446809aaab
Harry Chen <i@harrychen.xyz>
2019-01-29 23:01:16 +0800

    Add toggle button to fold sidebar
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import './css/main.css';
 import NumberedList from './components/NumberedList';
 import ExpandableList from './components/ExpandableList';
 import { COURSE_FUNC_LIST, SETTINGS_FUNC_LIST, SUMMARY_FUNC_LIST } from './utils/SideBarItems';
+import ToggleButton from './components/ToggleButton';
 
 const TEST_COURSE_LIST = [
   {
@@ -18,38 +19,67 @@ const TEST_COURSE_LIST = [
   },
 ];
 
-const App = () => {
-  return (
-      <div>
-        <div className="pane-folder">
-          <NumberedList
-              name="通知汇总"
-              icon="thumbtack"
-              items={SUMMARY_FUNC_LIST}
-              numbers={{}}
-          />
-          <Divider/>
-          <ExpandableList
-              name="本学期课程"
-              icon="inbox"
-              items={TEST_COURSE_LIST}
-              subitems={COURSE_FUNC_LIST}
-          />
-          <Divider/>
-          <NumberedList
-              name="设置"
-              icon="wrench"
-              items={SETTINGS_FUNC_LIST}
-              numbers={{}}
-          />
-        </div>
-        <div className="pane-message" id="pane-message"/>
-        <div className="pane-content" id="pane-content">
-          <Iframe
-              url="welcome.html"
-          />
-        </div>
-      </div>);
-};
+class App extends React.Component<any, any> {
+
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      paneHidden: false,
+    };
+  }
+
+  public render() {
+
+    const folderClass = ['pane-folder'];
+    const messageClass = ['pane-message'];
+    const contentClass = ['pane-content'];
+
+    if (this.state.paneHidden) {
+      folderClass.push('pane-hidden');
+      messageClass.push('pane-hidden');
+      contentClass.push('pane-content-fullscreen');
+    }
+
+    return (
+        <div>
+          <div className={folderClass.join(' ')}>
+            <NumberedList
+                name="通知汇总"
+                icon="thumbtack"
+                items={SUMMARY_FUNC_LIST}
+                numbers={{}}
+            />
+            <Divider/>
+            <ExpandableList
+                name="本学期课程"
+                icon="inbox"
+                items={TEST_COURSE_LIST}
+                subitems={COURSE_FUNC_LIST}
+            />
+            <Divider/>
+            <NumberedList
+                name="设置"
+                icon="wrench"
+                items={SETTINGS_FUNC_LIST}
+                numbers={{}}
+            />
+          </div>
+          <div className={messageClass.join(' ')} id="pane-message"/>
+          <div className={contentClass.join(' ')} id="pane-content">
+            <ToggleButton
+                handler={this.toggleButtonHandler}
+            />
+            <Iframe
+                url="welcome.html"
+            />
+          </div>
+        </div>);
+  }
+
+  private toggleButtonHandler = () => {
+    this.setState({ paneHidden: !this.state.paneHidden });
+  }
+
+}
 
 ReactDOM.render(<App />, document.querySelector('#index'));
