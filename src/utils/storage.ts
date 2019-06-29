b3c5a34d49f12d98016ce85497a3b6f95425067d
Harry Chen <i@harrychen.xyz>
2019-06-29 19:27:30 +0800

    Show changelog when version changed
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -5,6 +5,7 @@ import {
   STORAGE_SALT,
 } from '../constants';
 import { cipher, decipher } from './crypto';
+import { setDetailUrl } from '../redux/actions/ui';
 
 export async function storeCredential(username: string, password: string) {
   const cipherImpl = cipher(STORAGE_SALT);
@@ -29,23 +30,27 @@ export async function getStoredCredential() {
 }
 
 export async function removeStoredCredential() {
-  await await browser.storage.local.remove([STORAGE_KEY_USERNAME, STORAGE_KEY_PASSWORD]);
+  await browser.storage.local.remove([STORAGE_KEY_USERNAME, STORAGE_KEY_PASSWORD]);
 }
 
-export async function versionMigrate(store: object) {
-  let oldVersion = (await browser.storage.local.get([STORAGE_KEY_VERSION]))[STORAGE_KEY_VERSION];
-  if (oldVersion === undefined) {
-    oldVersion = 'legacy';
-    // migrate from version < 4.0.0, clearing all data
-    await browser.storage.local.clear();
-  } else {
-    // for future migration
-  }
+export async function versionMigrate(store) {
 
-  // set stored version to current one
+  const oldVersion = (await browser.storage.local.get([STORAGE_KEY_VERSION]))[STORAGE_KEY_VERSION];
   const currentVersion = (await (await fetch('/manifest.json')).json()).version;
-  console.info(`Migrating from version ${oldVersion} to ${currentVersion}`);
-  await browser.storage.local.set({
-    [STORAGE_KEY_VERSION]: currentVersion,
-  });
+
+  if (oldVersion === undefined) {
+    // migrate from version < 4.0.0, clearing all data
+    console.info('Migrating from legacy version, all data cleaned');
+    await browser.storage.local.clear();
+  } else if (oldVersion !== currentVersion) {
+    // for future migration
+    store.dispatch(setDetailUrl('changelog.html'));
+    // set stored version to current one
+    console.info(`Migrating from version ${oldVersion} to ${currentVersion}`);
+    await browser.storage.local.set({
+      [STORAGE_KEY_VERSION]: currentVersion,
+    });
+  } else {
+    console.info('Migration not necessary.');
+  }
 }
