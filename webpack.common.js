768ca5dce945c493d5c91ff535857f11c7a62995
Harry Chen <i@harrychen.xyz>
2020-01-01 00:24:14 +0800

    Upgrade yarn packages, fix webpack configuration
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- webpack.common.js
+++ webpack.common.js
@@ -40,13 +40,12 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', {
-          loader: 'typings-for-css-modules-loader',
+        use: [ 'style-loader', 'css-modules-typescript-loader', {
+          loader: 'css-loader',
           options: {
-            modules: true,
-            namedExport: true,
-            sass: true,
-            localIdentName: '[name]--[local]--[hash:base64:5]'
+            modules: {
+              localIdentName: '[name]--[local]--[hash:base64:5]'
+            }
           }
         }]
       },
