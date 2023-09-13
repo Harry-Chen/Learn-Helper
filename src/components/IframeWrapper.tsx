923979a220f5415b2362f698cf649745ab8c387d
AsakuraMizu <asakuramizu111@gmail.com>
2023-09-13 16:36:53 +0800

    fix: prevent iframe closing page

--- src/components/IframeWrapper.tsx
+++ src/components/IframeWrapper.tsx
@@ -15,7 +15,12 @@ const IframeWrapper = ({ id, className, url }: IframeWrapperProps) => {
 
   return (
     <Fragment key={url}>
-      <Iframe id={id} className={className} url={addCSRFTokenToIframeUrl(csrfToken, url)}></Iframe>
+      <Iframe
+        id={id}
+        className={className}
+        url={addCSRFTokenToIframeUrl(csrfToken, url)}
+        sandbox={['allow-forms', 'allow-modals', 'allow-popups', 'allow-scripts', 'allow-same-origin']}
+      ></Iframe>
     </Fragment>
   );
 };
