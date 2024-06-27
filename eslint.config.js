b110f41882a70f6b20221fd162c1a1a8d6612f5c
AsakuraMizu <asakuramizu111@gmail.com>
2024-06-28 03:06:52 +0800

    migrate to eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  jsxA11y.flatConfigs.recommended,
+  reactRecommended,
+  {
+    ignores: ['design', 'dist', '.wxt'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+);
